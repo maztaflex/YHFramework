1f8d0dfd2dda717ad7623f8f9710443c787b8f4f
Yongwhi <yonghwinam@smtown.com>
2016-04-12 14:12:17 +0900

    create project

---
+++ YHFramework/YHFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHFramework
+//
+//  Created by DEV_TEAM1_IOS on 2016. 4. 12..
+//  Copyright © 2016년 DoozerStage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YHFramework/YHFramework/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YHFramework
+//
+//  Created by DEV_TEAM1_IOS on 2016. 4. 12..
+//  Copyright © 2016년 DoozerStage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
