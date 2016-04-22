5479cecd47f2b3f859dcb43803b48294a70c496f
Yongwhi <yonghwinam@smtown.com>
2016-04-22 16:47:31 +0900

    post url session task

--- YHFramework/YHFramework/YHDURLSessionViewController.m
+++ YHFramework/YHFramework/YHDURLSessionViewController.m
@@ -8,24 +8,51 @@
 
 #import "YHDURLSessionViewController.h"
 
-#define YHDMemebershipStoreAPI                         @"https://pointapibeta.smtown.com/api/v1/brand"
+#define YHDMembershipStoreAPI                         @"https://pointapibeta.smtown.com/api/v1/brand"           // GET
+#define YHDCelebAuthorizeAPI                          @"https://pointapibeta.smtown.com/api/v1/introAuthCode"    // POST
 
 @interface YHDURLSessionViewController ()
+
+@property (strong, nonatomic) NSURLSession *urlSession;
 
 @end
 
 @implementation YHDURLSessionViewController
 
+#pragma mark - View Life Cycle
+
+// 1.
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        _urlSession = [NSURLSession sharedSession];
+    }
+    
+    return self;
+}
+
+// 2.
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+// 3.
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.title = @"URLSessionTask";
     
-    NSURLSession *urlSession = [NSURLSession sharedSession];
-    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:YHDMemebershipStoreAPI]];
+    [self testPostMethod];
+}
+
+#pragma mark - Test
+- (void)testGetMethod
+{
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:YHDMembershipStoreAPI]];
     
-    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-    
+    NSURLSessionDataTask *dataTask = [self.urlSession dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
         id jsonObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
         
         NSDictionary *dicJsonObject = nil;
@@ -40,19 +67,53 @@
     [dataTask resume];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)testPostMethod
+{
+    LogGreen(@"self.urlSession : %@",self.urlSession);
+    
+    NSDictionary *parameters = @{@"authCode" : @"1234"};
+    
+    NSURLRequest *req = [self generateRequestWithURLString:YHDCelebAuthorizeAPI httpMethod:@"POST" parameters:parameters];
+    
+    NSURLSessionDataTask *dataTask = [self.urlSession dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        id jsonObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        
+        NSDictionary *dicJsonObject = nil;
+        
+        if ([jsonObject isKindOfClass:[NSDictionary class]]) {
+            dicJsonObject = jsonObject;
+        }
+        
+        LogGreen(@"dicJsonObject : %@",dicJsonObject);
+    }];
+    
+    [dataTask resume];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Private Method
+- (NSURLRequest *)generateRequestWithURLString:(NSString *)urlString
+                                    httpMethod:(NSString *)httpMethod
+                                    parameters:(NSDictionary *)parameters
+{
+    NSMutableURLRequest *urlRequest = nil;
+    
+    NSData *bodyData = nil;
+    
+    urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+    
+    if ([NSJSONSerialization isValidJSONObject:parameters]) {
+        LogGreen(@"valid object!");
+        bodyData = [NSJSONSerialization dataWithJSONObject:parameters options:0 error:nil];
+    } else {
+        LogGreen(@"not valid object!");
+    }
+    
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [urlRequest setHTTPMethod:httpMethod];
+    [urlRequest setHTTPBody:bodyData];
+    
+    return urlRequest;
 }
-*/
 
 @end
